f74211924b8a76aac55b136d0102686d6d6305d3
Nirawit Jeein <nirawit.jee@gmail.com>
2025-05-08 10:02:48 +0700

    Python_Division

---
+++ introduction/Python_Division.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+    integer_result = a//b
+    float_result = a/b
+    
+    print(integer_result, float_result, sep = "\n")
